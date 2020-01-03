3d08fe048dee5d7ecc6c8e5d5c7b7d57450ccf8d
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-04 00:20:26 +0530

    first step

---
+++ 17000017.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int n,d,m,y;
+char fNumber[10];
+char dAirport[20];
+char aAirport[20];
+
+int main(){
+	cout<<"~~~~~~ Welcome to Virgin Airline ~~~~~~~"<<endl;
+	cout<<"Enter departure airport :-";
+	cin>>dAirport;
+	cout<<"Enter arrival airport :-";
+	cin>>aAirport;
+	
+	cout<<"Enter flight number :- ";
+	cin>>fNumber;
+   int d;
+   int m;
+   int y;
+//   cout<<"enter date and / "<<endl;
+//   std::cin >> d; // read the day
+//   if ( std::cin.get() != '/' ) // make sure there is a slash between DD and MM
+//   {
+//      std::cout << "You entered wrong format/\n";
+//      return 1;
+//   }
+//    std::cin >> m; // read the month
+//   if ( std::cin.get() != '/' ) // make sure there is a slash between MM and YYYY
+//   {
+//      std::cout << "You entered wrong format /\n";
+//      return 1;
+//   }
+//   std::cin >> y; // read the year
+//   std::cout << "input date: " << d << "/" << m << "/" << y << "\n";
+	cout<<"Enter date and time(DD/MM/YYYY)"<<endl;
+	cin>>d;
+	cout>>"/";
+	cin>>m;
+	cout>>"/";
+	cin>>y;
+	cout>>"/";
+	
+	
+	
+//	switch(n){
+//		case 1:
+//		
+//		
+//	
+//	
+//	}
+
+}
