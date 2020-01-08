719c8d2dff9a7414777443e8283ea32709a26fee
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-08 12:05:34 +0530

    adding some features

--- 17000017.cpp
+++ 17000017.cpp
@@ -4,20 +4,25 @@
 using namespace std;
 int main (){
   string array[100];
-  short loop;   
   string line; 
   int i=0,j=0,k=10;                        
   ifstream myfile ("Flights.txt"); 
   if (myfile.is_open()){            
-    while (! myfile.eof() )       
-    {
+    while (! myfile.eof()){
       getline (myfile,line);        
       cout << line <<  endl;            
-           for(loop=0; loop<1;loop++){     
-      			cout << array[loop];  
-      		};
       i++;	
 	}
+    while(k!=5){
+		cout<<"1.Display available flights"<<endl;
+		cout<<"2.View flight"<<endl;
+		cout<<"3.Seat availability"<<endl;
+		cout<<"4.Seat booking"<<endl;
+		cout<<"5.Exit"<<endl;
+		cout<<"Enter your choice : ";
+		cin>>k;
+ 	}
+
 	int size =i;
 	int start=i;
 	while(j<=size){
@@ -28,24 +33,26 @@ int main (){
 		//string arr[]=flight(array,start,end);
 		i++;
 	}
-    myfile.close();                         //closing the file  
+		
+    myfile.close();      
   }
   else{
-   cout << "Unable to open file";       //if the file is not open output
-  } 
+   cout << "Unable to open file"; 
+  }
+ 
 //  system("PAUSE");
 //    
 //       system("PAUSE");
 //  
-while(k!=5){
-	cout<<"1.Display available flights"<<endl;
-	cout<<"2.View flight"<<endl;
-	cout<<"3.Seat availability"<<endl;
-	cout<<"4.Seat booking"<<endl;
-	cout<<"5.Exit"<<endl;
-	cout<<"Enter your choice : ";
-	cin>>k;
-}
+class flight{
+	private string flightNumber;
+	private string date;
+	private string time;
+	
+};
+
+
+
 return 0; 
 }
 
