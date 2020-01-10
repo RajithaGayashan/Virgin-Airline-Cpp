0317bacf95ec8202dc69309bcd2a7743dfe3036f
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-10 14:30:41 +0530

    adding some features

--- 17000017.cpp
+++ 17000017.cpp
@@ -2,6 +2,38 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+class flight{
+	 string flightNumber;
+	 string date;
+	 string time;
+	 string depAirport;
+	 string arrAirport;
+	 bool ESeat[60][6];
+	 bool BSeat[60][6];
+
+	public:
+		flight(string array[],int start,int end){
+		flightNumber=array[start];
+		cout<<flightNumber<<endl;
+		string temp=array[start+1];
+		cout<<temp;
+		int i=0;
+		while(temp[i]!=" "){
+			this->depAirport=array[start+2];
+			this->arrAirport=array[start+3];
+		
+			for(int i=1;i<=60;i++){
+				for(int j=0;j<=5;j++){
+					ESeat[i][j]=false;
+					BSeat[i][j]=false;
+					int i=start+4;
+				}
+			}
+		}
+	}	
+};
+
 int main (){
   string array[100];
   string line; 
@@ -9,32 +41,35 @@ int main (){
   ifstream myfile ("Flights.txt"); 
   if (myfile.is_open()){            
     while (! myfile.eof()){
-      getline (myfile,line);        
-      cout << line <<  endl;            
+      getline (myfile,line); 
+	  array[i] = line;       
+      //cout << line <<  endl;            
       i++;	
 	}
-    while(k!=5){
+    /*while(k!=5){
 		cout<<"1.Display available flights"<<endl;
 		cout<<"2.View flight"<<endl;
 		cout<<"3.Seat availability"<<endl;
 		cout<<"4.Seat booking"<<endl;
 		cout<<"5.Exit"<<endl;
-		cout<<"Enter your choice : ";
+		cout<<"Enter your choice :";
 		cin>>k;
- 	}
-
-	int size =i;
-	int start=i;
+ 	}*/
+ 	
+	int size=i;
+	int i=0;
 	while(j<=size){
-		while(array[i]!="\n"){
+		int start=i;
+		while(array[i]!=""){
 			i++;
 		}
 		int end=i-1;
-		//string arr[]=flight(array,start,end);
+		flight f1(array,start,end);
 		i++;
+		j = i;
 	}
 		
-    myfile.close();      
+ //   myfile.close();      
   }
   else{
    cout << "Unable to open file"; 
@@ -43,16 +78,15 @@ int main (){
 //  system("PAUSE");
 //    
 //       system("PAUSE");
-//  
-class flight{
-	private string flightNumber;
-	private string date;
-	private string time;
+
 	
-};
-
-
-
+	//class fight(array,start,end);
+	
+//  
 return 0; 
 }
+
+
+
+
 
